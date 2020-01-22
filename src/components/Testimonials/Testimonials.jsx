5deaf03f088c1ffafec27ec88902a951b1252140
DimaMakarenko <makarenko.d.viri@gmail.com>
2020-01-22 16:25:39 +0200

    Testimonials: components, styles

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -9,6 +9,8 @@ import face4 from 'image/face4.png';
 import face5 from 'image/face5.png';
 import face6 from 'image/face6.png';
 import face7 from 'image/face7.png';
+import arrowLeftIcon from 'image/arrow-left.png';
+import arrowRightIcon from 'image/arrow-right.png';
 // styles
 import classNames from 'classnames/bind';
 import styles from './styles.module.scss';
@@ -22,14 +24,18 @@ const {
   testimonialName,
   testimonialCompany,
   testimonialUser,
-  testimonialFaces,
-  testimonialFace1,
-  testimonialFace2,
-  testimonialFace3,
-  testimonialFace4,
-  testimonialFace5,
-  testimonialFace6,
-  testimonialFace7
+  faces,
+  faceItems,
+  faceItem1,
+  faceItem2,
+  faceItem3,
+  faceItem4,
+  faceItem5,
+  faceItem6,
+  faceItem7,
+  arrows,
+  arrowLeft,
+  arrowRight
 } = styles;
 
 const Testimonials = ({ text, name, company, image }) => {
@@ -37,21 +43,25 @@ const Testimonials = ({ text, name, company, image }) => {
     <section className={testimonial}>
       <div className={cn('container', { testimonialContainers })}>
         <div className={avatar}>
-          <img src={image} alt="" />
+          <img src={image} alt=""/>
         </div>
         <span className={testimonialText}>{text}</span>
         <div className={testimonialUser}>
           <span className={testimonialName}>{name}</span>
           <span className={testimonialCompany}>{company}</span>
         </div>
-        <div className={testimonialFaces}>
-          <img src={face1} alt="" className={testimonialFace1} />
-          <img src={face2} alt="" className={testimonialFace2} />
-          <img src={face3} alt="" className={testimonialFace3} />
-          <img src={face4} alt="" className={testimonialFace4} />
-          <img src={face5} alt="" className={testimonialFace5} />
-          <img src={face6} alt="" className={testimonialFace6} />
-          <img src={face7} alt="" className={testimonialFace7} />
+        <div className={faces}>
+          <img src={face1} alt="" className={cn(faceItems, faceItem1)}/>
+          <img src={face2} alt="" className={cn(faceItems, faceItem2)}/>
+          <img src={face3} alt="" className={cn(faceItems, faceItem3)}/>
+          <img src={face4} alt="" className={cn(faceItems, faceItem4)}/>
+          <img src={face5} alt="" className={cn(faceItems, faceItem5)}/>
+          <img src={face6} alt="" className={cn(faceItems, faceItem6)}/>
+          <img src={face7} alt="" className={cn(faceItems, faceItem7)}/>
+        </div>
+        <div className={arrows}>
+          <img src={arrowLeftIcon} alt="" className={arrowLeft}/>
+          <img src={arrowRightIcon} alt="" className={arrowRight}/>
         </div>
       </div>
     </section>
