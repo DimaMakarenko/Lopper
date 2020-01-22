233b8bd62e5bf7734c1f03e65bc7eb9d8b99d06d
DimaMakarenko <43251210+DimaMakarenko@users.noreply.github.com>
2020-01-22 10:06:20 +0200

    NavBar: css-modules, styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,46 +1,48 @@
 import React from 'react';
+// proptypes
+import PropTypes from 'prop-types';
 // routing
 import { Link } from 'react-router-dom';
 // styles
 import classNames from 'classnames/bind';
-import phoneIco from 'image/phone.svg';
-import styles from './styles.scss';
-// image
+import phoneIcoBlack from 'image/phone-black.svg';
+import phoneIcoWhite from 'image/phone.svg';
+import styles from './styles.module.scss';
+
 const cn = classNames.bind(styles);
+const { navBar, logo, navBarWhite, menuItem, burgerIcon, burger, phone, burgerIconWhite } = styles;
 
 const NavBar = ({ isWhite }) => {
   return (
-    <div
-      className={cn('container nav-bar', {
-        isWhite
-      })}
-    >
-      <div className="logo">Lopper</div>
-      <nav className={cn('menu', { isWhite } && 'menu-white')}>
-        <Link to="/home" className="menu-item">
+    <div className={cn('container', { navBar }, isWhite && navBarWhite)}>
+      <div className={logo}>Lopper</div>
+      <nav>
+        <Link to="/home" className={menuItem}>
           Home
         </Link>
-        <Link to="/services" className="menu-item">
+        <Link to="/services" className={menuItem}>
           Services
         </Link>
-        <Link to="/work" className="menu-item">
+        <Link to="/work" className={menuItem}>
           Work
         </Link>
-        <Link to="/about" className="menu-item">
+        <Link to="/about" className={menuItem}>
           About
         </Link>
       </nav>
-      <div className="phone">
-        <div className="phone-image">
-          <img src={phoneIco} alt="phone" />
-        </div>
-        <div className="phone-number">(01) 666 - 693 - 456</div>
+      <div className={phone}>
+        <img src={isWhite ? phoneIcoWhite : phoneIcoBlack} alt="phone" />
+        <div>(01) 666 - 693 - 456</div>
       </div>
-      <div className="burger">
-        <span className={cn('burger-icon', { isWhite } && 'burger-white')} />
+      <div className={burger}>
+        <span className={cn({ burgerIcon }, isWhite && burgerIconWhite)} />
       </div>
     </div>
   );
+};
+
+NavBar.propTypes = {
+  isWhite: PropTypes.bool.isRequired
 };
 
 export default NavBar;
