a2ad8f39cd76bdb2c9a6c10b7a59d31fe3bcc080
DimaMakarenko <43251210+DimaMakarenko@users.noreply.github.com>
2020-01-22 15:49:37 +0200

    Testimonials: styles

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 // components
 import Header from 'components/Header/Header';
-// styles
+import Testimonials from 'components/Testimonials/Testimonials';
+// styles/image
 import classNames from 'classnames/bind';
-import styles from './styles.scss';
-// image
 import headerImage from 'image/about-header.png';
+import testimonialsAvatar from 'image/avatar1.png';
+import styles from './styles.scss';
 
 const cn = classNames.bind(styles);
-
+// header
 const title = 'About us';
 const subTitle = 'We build great business';
+// testimonials
+const testimonialsText =
+  'Lectus arcu bibendum at varius. Adipiscing diam donec adipiscing tristique.';
+const testimonialsName = 'Kerry Johnes';
+const testimonialsCompany = 'Some Company';
 
 const About = () => {
   return (
     <section className={cn('about')}>
       <Header title={title} subTitle={subTitle} image={headerImage} />
+      <Testimonials
+        text={testimonialsText}
+        name={testimonialsName}
+        company={testimonialsCompany}
+        image={testimonialsAvatar}
+      />
     </section>
   );
 };
