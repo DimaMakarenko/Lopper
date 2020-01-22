0bad7dc5031cd3c79f853b2ac74fbbe5b0fa0f29
DimaMakarenko <makarenko.d.viri@gmail.com>
2020-01-22 17:43:33 +0200

    Subscribe: components, styles

---
+++ components/Subscribe/Subscribe.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+// image
+import arrowRightIcon from 'image/arrow-right.png';
+// styles
+import classNames from 'classnames/bind';
+import styles from './styles.module.scss';
+
+const cn = classNames.bind(styles);
+const { subscribe, title, subTitle, form, formBtn, formInput, formImage } = styles;
+
+const Subscribe = () => {
+  return (
+    <section className={cn('container', subscribe)}>
+      <h2 className={title}>Subscribe to our newsletter</h2>
+      <span className={subTitle}>Cras pulvinar mattis nunc sed blandit.</span>
+      <form className={form}>
+        <input type="text" placeholder="Enter email" className={formInput} />
+        <span className={formBtn}>
+          Subscribe
+          <img src={arrowRightIcon} alt="" className={formImage} />
+        </span>
+      </form>
+    </section>
+  );
+};
+
+export default Subscribe;
