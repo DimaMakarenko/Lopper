1c421d9dc7ebbe7b45d48dcf796f0e561685bdac
DimaMakarenko <makarenko.d.viri@gmail.com>
2020-01-24 09:48:51 +0200

    Home: content, styles

--- pages/Home/component/Header/Header.jsx
+++ pages/Home/component/Header/Header.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
-// image
+// component
 import NavBar from 'components/NavBar/NavBar';
+// image
+import scrollIcon from 'image/scroll.svg';
+import arrowCircle from 'image/arrow-circle.svg';
 // styles
 import classNames from 'classnames/bind';
 import styles from './styles.module.scss';
 
 const cn = classNames.bind(styles);
+const {
+  header,
+  headerContent,
+  pink,
+  headerTitle,
+  headerSubtitle,
+  headerScroll,
+  scrollImage,
+  subheader,
+  subImage
+} = styles;
 
 const Header = () => {
   return (
-    <>
+    <div className={cn('container', header)}>
       <NavBar isWhite />
-    </>
+      <div className={headerContent}>
+        <span className={headerTitle}>
+          <span className={headerSubtitle}>What we do?</span>
+          We grow
+          <span className={pink}>great business</span>
+        </span>
+        <div className={headerScroll}>
+          Scroll to see more
+          <img src={scrollIcon} alt="" className={scrollImage} />
+        </div>
+      </div>
+      <div className={subheader}>
+        Velit laoreet id donec ultrices. Ut lectus arcu bibendum varius.
+        <img src={arrowCircle} alt="" className={subImage} />
+      </div>
+    </div>
   );
 };
 
