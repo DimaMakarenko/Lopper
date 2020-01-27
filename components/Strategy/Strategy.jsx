083aee74ce79922fe7ef69859ad04d1c4b0d9d30
DimaMakarenko <makarenko.d.viri@gmail.com>
2020-01-27 10:03:55 +0200

    Strategy: components, styles

--- components/Strategy/Strategy.jsx
+++ components/Strategy/Strategy.jsx
@@ -10,9 +10,6 @@ import classNames from 'classnames/bind';
 import styles from './styles.module.scss';
 
 const cn = classNames.bind(styles);
-const {
-
-} = styles;
 // intro
 const title = 'What we  do';
 const text =
@@ -22,7 +19,49 @@ const Strategy = () => {
   return (
     <section className={cn('block')}>
       <Intro subTitle={title} title={title} text={text} position="center" />
-
+      <div className={cn('strategy')}>
+        <div className={cn('items')}>
+          <div className={cn('itemTextContainer')}>
+            <span className={cn('textContainer', 'itemSubTitle', 'pinkText')}>01</span>
+            <span className={cn('textContainer', 'itemTitle')}>
+              Upper prior hundred links approach reedy
+            </span>
+            <span className={cn('textContainer', 'itemText')}>
+              Upper prior hundred links approach reedy, was to the than and the ever somehow
+              surprised known for every by of there until road,
+            </span>
+          </div>
+          <div className={cn('itemImageContainer', 'pinkElement')}>
+            <img src={strategyImage1} alt="" className={cn('itemImage')} />
+          </div>
+          <div className={cn('itemImageContainer', 'greenElement')}>
+            <img src={strategyImage2} alt="" className={cn('itemImage')} />
+          </div>
+          <div className={cn('itemTextContainer')}>
+            <span className={cn('textContainer', 'itemSubTitle', 'greenText')}>02</span>
+            <span className={cn('textContainer', 'itemTitle')}>
+              Upper prior hundred links approach reedy
+            </span>
+            <span className={cn('textContainer', 'itemText')}>
+              Upper prior hundred links approach reedy, was to the than and the ever somehow
+              surprised known for every by of there until road,
+            </span>
+          </div>
+          <div className={cn('itemTextContainer')}>
+            <span className={cn('textContainer', 'itemSubTitle', 'purpleText')}>03</span>
+            <span className={cn('textContainer', 'itemTitle')}>
+              Upper prior hundred links approach reedy
+            </span>
+            <span className={cn('textContainer', 'itemText')}>
+              Upper prior hundred links approach reedy, was to the than and the ever somehow
+              surprised known for every by of there until road,
+            </span>
+          </div>
+          <div className={cn('itemImageContainer', 'purpleElement')}>
+            <img src={strategyImage3} alt="" className={cn('itemImage')} />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
