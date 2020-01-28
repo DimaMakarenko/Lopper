4c62197d75606e7faefff0559ad70ef9af51bb7a
DimaMakarenko <makarenko.d.viri@gmail.com>
2020-01-28 17:25:23 +0200

    Updates

---
+++ precache-manifest.f276d0dca67726cc9f27555f962895d3.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45fc7e7ea4e1fab516dc7332709b8832",
+    "url": "/Lopper/index.html"
+  },
+  {
+    "revision": "d12662b4f6db91025b7c",
+    "url": "/Lopper/static/css/main.fe41d177.chunk.css"
+  },
+  {
+    "revision": "51cb713d8199cbd2d945",
+    "url": "/Lopper/static/js/2.3cdd4b26.chunk.js"
+  },
+  {
+    "revision": "738c8d32191c93cca78350c0114a36fa",
+    "url": "/Lopper/static/js/2.3cdd4b26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d12662b4f6db91025b7c",
+    "url": "/Lopper/static/js/main.90482e4c.chunk.js"
+  },
+  {
+    "revision": "c0370b0bbb1b4448bf28",
+    "url": "/Lopper/static/js/runtime-main.6769eae5.js"
+  },
+  {
+    "revision": "3a6332d25e875515d7d344e1babe0ed0",
+    "url": "/Lopper/static/media/about-header.3a6332d2.png"
+  },
+  {
+    "revision": "3a5c7c7a9216b9b232bb441faf66d499",
+    "url": "/Lopper/static/media/arrow-circle.3a5c7c7a.svg"
+  },
+  {
+    "revision": "6d972b86adb39402538b263f240db5f4",
+    "url": "/Lopper/static/media/avatar.mini.6d972b86.png"
+  },
+  {
+    "revision": "e0c85a7f0da1d9f82418e5719a9ce730",
+    "url": "/Lopper/static/media/avatar1.e0c85a7f.png"
+  },
+  {
+    "revision": "f35586813f8df4dd5b63c15ee2c81ca8",
+    "url": "/Lopper/static/media/avatar2.f3558681.png"
+  },
+  {
+    "revision": "234fd81fe5ab21ef09f7cda4d24991a1",
+    "url": "/Lopper/static/media/avatar3.234fd81f.png"
+  },
+  {
+    "revision": "2af12fd4fa54b10d6658ecde5f0a377c",
+    "url": "/Lopper/static/media/avatar4.2af12fd4.png"
+  },
+  {
+    "revision": "2bef1a29a2cc0765ac6f1ad0c56a2d65",
+    "url": "/Lopper/static/media/avatar5.2bef1a29.png"
+  },
+  {
+    "revision": "2e076022a66e6a4341f5a91bec1d1dcb",
+    "url": "/Lopper/static/media/avatar6.2e076022.png"
+  },
+  {
+    "revision": "8f0ebf60398717c19c441920d43c0f37",
+    "url": "/Lopper/static/media/earth.8f0ebf60.svg"
+  },
+  {
+    "revision": "d0397df4efe27566d136ba7f69b58a72",
+    "url": "/Lopper/static/media/face1.d0397df4.png"
+  },
+  {
+    "revision": "eb32695fdaee31da08cd01ef21ba5a02",
+    "url": "/Lopper/static/media/face2.eb32695f.png"
+  },
+  {
+    "revision": "4e2eb0e790887d8896494c9c40797779",
+    "url": "/Lopper/static/media/face3.4e2eb0e7.png"
+  },
+  {
+    "revision": "77f247da03e62302f46a035f0dd144fa",
+    "url": "/Lopper/static/media/face4.77f247da.png"
+  },
+  {
+    "revision": "6e3d1cfb85f614fd55a094b83d47e5fd",
+    "url": "/Lopper/static/media/face5.6e3d1cfb.png"
+  },
+  {
+    "revision": "d42baeeb40f292c846b1d15fa8ef684c",
+    "url": "/Lopper/static/media/face6.d42baeeb.png"
+  },
+  {
+    "revision": "0c4b154a55d4f65a05f1ec1241d47744",
+    "url": "/Lopper/static/media/face7.0c4b154a.png"
+  },
+  {
+    "revision": "974c4fbfee66d9ad619905f01dcb2eb7",
+    "url": "/Lopper/static/media/facebook.974c4fbf.svg"
+  },
+  {
+    "revision": "f4b630bc221db4b5a50c871ef92c3b2f",
+    "url": "/Lopper/static/media/features1.f4b630bc.png"
+  },
+  {
+    "revision": "6395c7c4a44dc75b67a2141937ac9c86",
+    "url": "/Lopper/static/media/features2.6395c7c4.png"
+  },
+  {
+    "revision": "6158091f19e9033a36cd286f37f3e6e4",
+    "url": "/Lopper/static/media/gallery1.6158091f.png"
+  },
+  {
+    "revision": "15adab5190887695360cd42f682da842",
+    "url": "/Lopper/static/media/gallery2.15adab51.png"
+  },
+  {
+    "revision": "a55f244cd7c8007dea8939373c93b53b",
+    "url": "/Lopper/static/media/gallery3.a55f244c.png"
+  },
+  {
+    "revision": "2bb9dbfb9628166a553864a26db9446f",
+    "url": "/Lopper/static/media/gallery4.2bb9dbfb.png"
+  },
+  {
+    "revision": "747161d2e6448f723641a1719f55c14c",
+    "url": "/Lopper/static/media/gallery5.747161d2.png"
+  },
+  {
+    "revision": "f4cb735efd1df223e0db113a785b69cc",
+    "url": "/Lopper/static/media/home-header.f4cb735e.png"
+  },
+  {
+    "revision": "bf39bc0a45d831ad9174b29ce1bc3ddc",
+    "url": "/Lopper/static/media/home-man.bf39bc0a.png"
+  },
+  {
+    "revision": "d0d98f5132f839fbfd00e67709d294d3",
+    "url": "/Lopper/static/media/icon1.d0d98f51.svg"
+  },
+  {
+    "revision": "6b396a15e6acd8289196829ab15208a5",
+    "url": "/Lopper/static/media/icon2.6b396a15.svg"
+  },
+  {
+    "revision": "3686fdbf950e2d5338a44b272e16227e",
+    "url": "/Lopper/static/media/icon3.3686fdbf.svg"
+  },
+  {
+    "revision": "5342e1f29c5df51ab46d33ca4972e836",
+    "url": "/Lopper/static/media/icon4.5342e1f2.svg"
+  },
+  {
+    "revision": "7c6012dda8219e095956ef9f211ec01b",
+    "url": "/Lopper/static/media/icon5.7c6012dd.svg"
+  },
+  {
+    "revision": "ff3cb1cd3058c6c0d4030313cc2e012e",
+    "url": "/Lopper/static/media/icon6.ff3cb1cd.svg"
+  },
+  {
+    "revision": "341a90a30d51f27cb777a6cf16b884dd",
+    "url": "/Lopper/static/media/instagram.341a90a3.svg"
+  },
+  {
+    "revision": "a47071afc44ec53d6ce8989c8cfe5d3e",
+    "url": "/Lopper/static/media/mail.a47071af.svg"
+  },
+  {
+    "revision": "c6f7e3845101f06aae699205cdee1b0b",
+    "url": "/Lopper/static/media/medium.c6f7e384.svg"
+  },
+  {
+    "revision": "c56471dcb5b218288a1e740abb272768",
+    "url": "/Lopper/static/media/phone-black.c56471dc.svg"
+  },
+  {
+    "revision": "92a99728ced2d3113812141971b954e0",
+    "url": "/Lopper/static/media/phone.92a99728.svg"
+  },
+  {
+    "revision": "24c68854f8f424f7189b72626a60c5eb",
+    "url": "/Lopper/static/media/phone.min.24c68854.svg"
+  },
+  {
+    "revision": "bdc173117f1a2934d67c9417c00a8504",
+    "url": "/Lopper/static/media/react.bdc17311.svg"
+  },
+  {
+    "revision": "f5b5add67b37567965ba97718518ad6b",
+    "url": "/Lopper/static/media/react.f5b5add6.svg"
+  },
+  {
+    "revision": "04e7aba4aa0868c7ad912ca7b3e4c075",
+    "url": "/Lopper/static/media/scroll.04e7aba4.svg"
+  },
+  {
+    "revision": "7ad61c3b844c38c2add0576867d3d9d8",
+    "url": "/Lopper/static/media/services-header.7ad61c3b.png"
+  },
+  {
+    "revision": "17f15d5a9325df9365b7e91beb62990b",
+    "url": "/Lopper/static/media/strategy1.17f15d5a.png"
+  },
+  {
+    "revision": "20dbe34a8820063144707e52b75cb437",
+    "url": "/Lopper/static/media/strategy2.20dbe34a.png"
+  },
+  {
+    "revision": "59935d716945da88c55165ac5bfaed47",
+    "url": "/Lopper/static/media/strategy3.59935d71.png"
+  },
+  {
+    "revision": "3f47b8e5a4c3cdd8b0c2094817ac9b55",
+    "url": "/Lopper/static/media/team-header.3f47b8e5.png"
+  },
+  {
+    "revision": "feeef06c26892c664ef4479a85445068",
+    "url": "/Lopper/static/media/twitter.feeef06c.svg"
+  },
+  {
+    "revision": "2fdf2f14daf66404b85d31de9493afee",
+    "url": "/Lopper/static/media/virtues-bg.2fdf2f14.png"
+  },
+  {
+    "revision": "c7dce5a75269589dc1fa3e1f8c71fd55",
+    "url": "/Lopper/static/media/youtube.c7dce5a7.svg"
+  }
+]);
