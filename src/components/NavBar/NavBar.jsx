2480f289f341d4eb0784e850e1fad504fc11e5e3
DimaMakarenko <makarenko.d.viri@gmail.com>
2020-01-29 12:32:07 +0200

    NavBar: menu media query

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,19 +3,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // routing
 import { Link } from 'react-router-dom';
-// styles
-import classNames from 'classnames/bind';
+// image
 import phoneIcoBlack from 'image/phone-black.svg';
 import phoneIcoWhite from 'image/phone.svg';
+// styles
+import classNames from 'classnames/bind';
 import styles from './styles.module.scss';
 
 const cn = classNames.bind(styles);
 
 const NavBar = ({ colorText }) => {
+  const menuRef = React.createRef();
+  const burgerRef = React.createRef();
+
+  const openMenu = () => {
+    menuRef.current.classList.toggle(styles.openMenu);
+    burgerRef.current.classList.toggle(styles.openBurger);
+  };
   return (
     <div className={cn('container', 'navBar', colorText)}>
       <h2 className={cn('logo')}>Lopper</h2>
-      <nav className={cn('menu')}>
+      <nav className={cn('menu')} ref={menuRef}>
         <Link to="/home" className={cn('menuItem')}>
           Home
         </Link>
@@ -40,8 +48,28 @@ const NavBar = ({ colorText }) => {
           <a href="tel:01-666-693-456">(01) 666 - 693 - 456</a>
         </div>
       </div>
-      <div className={cn('burger')}>
-        <span className={cn('burgerIcon', colorText && 'burgerIconWhite')} />
+      <div className={cn('burger')} onClick={openMenu} ref={burgerRef}>
+        <span
+          className={cn(
+            'burgerItem',
+            'firstBurgerItem',
+            colorText === 'white' && 'burgerIconWhite'
+          )}
+        />
+        <span
+          className={cn(
+            'burgerItem',
+            'secondBurgerItem',
+            colorText === 'white' && 'burgerIconWhite'
+          )}
+        />
+        <span
+          className={cn(
+            'burgerItem',
+            'thirdBurgerItem',
+            colorText === 'white' && 'burgerIconWhite'
+          )}
+        />
       </div>
     </div>
   );
